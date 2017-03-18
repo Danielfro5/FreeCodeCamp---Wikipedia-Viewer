edfbee612b750621c006365e46c881eb9740cb6a
Danielfro5 <br3akcor3@yandex.com>
2017-03-18 17:49:50 +1100

    added cross origin me proxy

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json",function(wikiData){
-    console.log("atleast its not broken");
-  });
-  });
+    $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(wikiData) {
+        console.log("atleast its working");
+    });
+});
