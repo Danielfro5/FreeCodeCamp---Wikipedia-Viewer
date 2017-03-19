b7135769c3f23f535acc84112bef3478fd73fbfe
Danielfro5 <br3akcor3@yandex.com>
2017-03-20 04:23:12 +1100

    added search function to api string

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+var userSearchTerm = "test";
+
+//create textarea search box and feed input in realtime to userSearchTerm variable.
+
 $(function() {
 
-    $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(wikiData) {
-        console.log("atleast its working");
+    $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+(userSearchTerm), function(wikiData) {
+        console.log(wikiData);
     });
 });
