83d688cdaaefaaacc4a7ec1ddc49192e73530417
Danielfro5 <br3akcor3@yandex.com>
2017-03-23 00:36:21 +1100

    facelifted html/css

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-
 var userSearchString;
 var searchStringHolder;
 var searchStringMulti;
 
-function searchFunction(){
-  searchStringHolder = "";
-  searchStringMulti = "";
-  userSearchString = document.getElementById("searchBar").value;
-  $(function() {
+function searchFunction() {
+    searchStringHolder = "";
+    searchStringMulti = "";
+    userSearchString = document.getElementById("searchBar").value;
+    $(function() {
 
-      $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+(userSearchString), function(wikiData) {
-          var termLength = wikiData[1].length;
-          for (i=1;i<termLength;i++){
-            searchStringHolder =
-            "<h3>"+wikiData[1][i]+"</h3><br><p>"+wikiData[2][i]+"</p><br><a href='"+wikiData[3][i]+"'>"+wikiData[3][i]+"</a>";
-            searchStringMulti += searchStringHolder;
-            document.getElementById("searchResults").innerHTML = searchStringMulti;
-          }
+        $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + (userSearchString), function(wikiData) {
+            var termLength = wikiData[1].length;
+            for (i = 1; i < termLength; i++) {
+                searchStringHolder =
+                    "<h3>" + wikiData[1][i] + "</h3><br><p>" + wikiData[2][i] + "</p><br><a href='" + wikiData[3][i] + "'>" + wikiData[3][i] + "</a></div>";
+                searchStringMulti += searchStringHolder;
+                document.getElementById("searchResults").innerHTML = searchStringMulti;
+            }
 
-      });
-  });
+        });
+    });
 
 }
