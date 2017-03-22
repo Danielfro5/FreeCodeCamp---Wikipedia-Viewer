c63b4fd6587c19bbf14f810dc5a33d138e06ff2e
Danielfro5 <br3akcor3@yandex.com>
2017-03-22 23:52:05 +1100

    added correctly working searchbox

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var userSearchTerm = window.prompt("Enter Search Term","");
+
+var userSearchString;
 var searchStringHolder;
 var searchStringMulti;
 
-$(function() {
+function searchFunction(){
+  searchStringHolder = "";
+  searchStringMulti = "";
+  userSearchString = document.getElementById("searchBar").value;
+  $(function() {
 
-    $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+(userSearchTerm), function(wikiData) {
-        var termLength = wikiData.length;
+      $.getJSON("https:///crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+(userSearchString), function(wikiData) {
+          var termLength = wikiData[1].length;
+          for (i=1;i<termLength;i++){
+            searchStringHolder =
+            "<h3>"+wikiData[1][i]+"</h3><br><p>"+wikiData[2][i]+"</p><br><a href='"+wikiData[3][i]+"'>"+wikiData[3][i]+"</a>";
+            searchStringMulti += searchStringHolder;
+            document.getElementById("searchResults").innerHTML = searchStringMulti;
+          }
 
-        for (i=1;i<termLength;i++){
-          searchStringHolder =
-          "<h3>"+wikiData[1][i]+"</h3><br><p>"+wikiData[2][i]+"</p><br><a href='"+wikiData[3][i]+"'>"+wikiData[3][i]+"</a>";
-          searchStringMulti += searchStringHolder;
-          document.getElementById("searchResults").innerHTML = searchStringMulti;
+      });
+  });
 
-
-
-
-
-
-        }
-
-    });
-});
+}
