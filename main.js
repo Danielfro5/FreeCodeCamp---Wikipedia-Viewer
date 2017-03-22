0a1aed55c6cd531a737a96ad3547285369625a60
Danielfro5 <br3akcor3@yandex.com>
2017-03-23 04:23:59 +1100

    added enter key to search function/fixed css style

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function searchFunction() {
             var termLength = wikiData[1].length;
             for (i = 1; i < termLength; i++) {
                 searchStringHolder =
-                    "<h3>" + wikiData[1][i] + "</h3><br><p>" + wikiData[2][i] + "</p><br><a href='" + wikiData[3][i] + "'>" + wikiData[3][i] + "</a></div>";
+                    "<div><h3>" + wikiData[1][i] + "</h3><br><p>" + wikiData[2][i] + "</p><br><a href='" + wikiData[3][i] + "'>" + wikiData[3][i] + "</a></div>";
                 searchStringMulti += searchStringHolder;
                 document.getElementById("searchResults").innerHTML = searchStringMulti;
             }
@@ -21,3 +21,9 @@ function searchFunction() {
     });
 
 }
+
+$(function() {
+$('#searchBar').keypress(function(e){
+   if(e.keyCode==13)
+   $('#searchBarBtn').click();
+ });});
